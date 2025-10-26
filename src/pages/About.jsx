0feebed7e27479fd131f71f63225cf663193598c
ddobbinss <damiendobbins31@gmail.com>
2025-10-26 19:25:04 -0400

    deploy change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,14 +9,14 @@ export default function About() {
             <p>At Exotic Imports, our mission is to share the vibrant beauty of the underwater world with aquarists everywhere. We specialize in bringing rare and responsibly sourced exotic fish to your aquarium, ensuring every species is healthy, sustainable, and ethically cared for. Beyond just offering fish, we provide expert guidance and resources so that every customer — from beginner hobbyists to seasoned collectors</p>
         </div>
         <div className="aboutus-img">
-            <img src="./../images/large1.png"/>
+            <img src="./../images/large1.png" alt="fish"/>
         </div>
         <div id="why-div">
             <h1>Why</h1>
             <p>At Exotic Imports, our work goes beyond selling rare fish — it’s about a shared love for aquatic life. We believe aquariums are living art, and every species has a story worth celebrating. This marketplace was built as more than a storefront; it’s a space where enthusiasts, collectors, and caretakers come together over a common passion.</p>
         </div>
         <div className="aboutus-img">
-            <img src="./../images/large2.png"/>    
+            <img src="./../images/large2.png" alt="fish"/>    
         </div>
             <h2>News & Updates</h2>
         <div id="news-div">
